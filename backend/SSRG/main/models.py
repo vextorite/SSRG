8fd0ab3f59471af3cf0f8fcc3dafc77fa3a3314c
vextorite <HLNSAN005@myuct.ac.za>
2021-08-15 13:18:28 +0200

    init Job DB fields

--- backend/SSRG/main/models.py
+++ backend/SSRG/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Jobs(models.Model):
+    user = models.CharField(max_length=200)
+    userEmail = models.EmailField(max_length=200)
+    files = models.FileField(upload_to='jobs/')
+    uploadDate = models.DateTimeField(auto_now_add=True)
