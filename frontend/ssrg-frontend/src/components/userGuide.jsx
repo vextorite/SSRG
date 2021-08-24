6490d836077a30f188012b5d469d99ccb96a11e9
Sashen Naidoo <ndxsas021@cs.uct.ac.za>
2021-08-24 13:51:08 +0200

    cleaning up build

---
+++ frontend/ssrg-frontend/src/components/userGuide.jsx
@@ -0,0 +1,14 @@
+import {React, useState} from "react";
+import "../component-styles/jobStatus.css";
+
+export default function UserGuide(props) {
+
+  return (
+
+<div className="mainJobs">
+<button onClick={props.back} className="backBtn">Return to home</button>
+<div className="submitPageTitle">USER GUIDE</div>
+</div>
+  
+  );
+}
