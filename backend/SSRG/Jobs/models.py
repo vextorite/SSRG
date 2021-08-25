a69289b787bf15e4e3890c38e5ca49ac3ebaa213
vextorite <HLNSAN005@myuct.ac.za>
2021-08-26 00:35:37 +0200

    Database redesign init 1

--- backend/SSRG/Jobs/models.py
+++ backend/SSRG/Jobs/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Jobs(models.Model):
     LANGUAGE_CHOICES = [('java', 'Java',), ('py', 'Python'), ('cpp', 'C++')]
-    userEmail = models.EmailField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #userEmail = models.EmailField(max_length=200)
     files = models.FileField(upload_to='jobs/')
     uploadDate = models.DateTimeField(auto_now_add=True)
     language = models.CharField(max_length=50, choices=LANGUAGE_CHOICES)
     baseFile = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.userEmail+": "+str(self.uploadDate)
+        return self.user.username+"["+str(self.uploadDate)+"]"
