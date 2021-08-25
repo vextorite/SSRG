bd3620aa70d227e5259887c20b7ea0585d1e4d93
Sashen Naidoo <ndxsas021@cs.uct.ac.za>
2021-08-25 11:01:01 +0200

    job status components created - awaiting integration for further UI dev

--- frontend/ssrg-frontend/src/components/jobStatus.jsx
+++ frontend/ssrg-frontend/src/components/jobStatus.jsx
@@ -1,5 +1,6 @@
 import {React, useState} from "react";
 import "../component-styles/jobStatus.css";
+import JobComponent from './jobStatusDisplay.jsx';
 
 export default function JobStatus(props) {
 
@@ -7,7 +8,21 @@ export default function JobStatus(props) {
 
 <div className="mainJobs">
 <button onClick={props.back} className="backBtn">Return to home</button>
+<div>
 <div className="submitPageTitle">View job statuses</div>
+<JobComponent id="1" refn="2408154501" state="pending"></JobComponent>
+<JobComponent id="2" refn="2308195002" state="complete"></JobComponent>
+<JobComponent id="3" refn="2308195001" state="complete"></JobComponent>
+<JobComponent id="4" refn="2208172101" state="error"></JobComponent>
+<JobComponent id="5" refn="2208135201" state="complete"></JobComponent>
+<div className="key">
+  <div>
+  <div className="keyItem"><div className="keyItemTitle">Complete</div> <div className="complete"></div></div>
+  <div className="keyItem"><div className="keyItemTitle">Pending</div> <div className="pending"></div></div>
+  <div className="keyItem"><div className="keyItemTitle">Error</div> <div className="error"></div></div>
+  </div>
+</div>
+</div>
 </div>
   
   );
