288dbb6de34334b24163e4da0dd1937da2df4d28
vextorite <HLNSAN005@myuct.ac.za>
2021-08-26 01:36:38 +0200

    Submit Jobs implementation init

--- backend/SSRG/Jobs/forms.py
+++ backend/SSRG/Jobs/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import LANGUAGE_CHOICES, Jobs
 
 class NewUser(UserCreationForm):
     email = forms.EmailField(required=True)
 
-    class MetaData:
+    class Meta:
         model = User
-        fields = ("username", "email", "password", "passwordConf")
+        fields = ("username", "email", "password1", "password2")
 
     def save(self, commit=True):
         user = super(NewUser, self).save(commit=False)
@@ -15,3 +16,9 @@ class NewUser(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class submitJob(forms.Form):
+    language = forms.CharField(widget=forms.Select(choices=LANGUAGE_CHOICES), required=True)
+    files = forms.FileField(required=True)
+    baseFile = forms.CharField(required=False)
--- backend/SSRG/Jobs/models.py
+++ backend/SSRG/Jobs/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 # Create your models here.
+LANGUAGE_CHOICES = [('java', 'Java',), ('py', 'Python'), ('cpp', 'C++')]
 
 class Jobs(models.Model):
-    LANGUAGE_CHOICES = [('java', 'Java',), ('py', 'Python'), ('cpp', 'C++')]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     #userEmail = models.EmailField(max_length=200)
     files = models.FileField(upload_to='jobs/')
@@ -14,3 +15,8 @@ class Jobs(models.Model):
 
     def __str__(self):
         return self.user.username+"["+str(self.uploadDate)+"]"
+
+class JobForm(ModelForm):
+    class Meta:
+        model = Jobs
+        fields = ['language', 'files', 'baseFile']
