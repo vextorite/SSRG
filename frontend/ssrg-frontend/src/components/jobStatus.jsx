a4ddb17b8e46d9f8c5c78172bcebb8f31d38c481
Sashen Naidoo <ndxsas021@cs.uct.ac.za>
2021-08-26 23:19:00 +0200

    final changes

--- frontend/ssrg-frontend/src/components/jobStatus.jsx
+++ frontend/ssrg-frontend/src/components/jobStatus.jsx
@@ -16,7 +16,7 @@ export default function JobStatus(props) {
 <JobComponent id="2" refn="2308195002" state="complete" heading="Job Success" content="This submission report has been generated successfully. [Submitted Attachments: 'CSC3002F Assignment 6']"></JobComponent>
 <JobComponent id="3" refn="2308195001" state="complete" heading="Job Success" content="This submission report has been generated successfully. [Submitted Attachments: 'CSC3002F Assignment 5']"></JobComponent>
 <JobComponent id="4" refn="2208172101" state="error" heading="Job Error" content="This submission failed due to a MOSS error: MOSS302: 'file exceeded input limitations'. [Submitted Attachments: 'CSC3002F Assignment 4']"></JobComponent>
-<JobComponent id="5" refn="2208135201" state="complete" heading="Job Success" content="This submission report has been generated successfully. [Submitted Attachments: 'CSC3002F Assignment 3']"></JobComponent>
+<JobComponent id="5" refn="2208135201" state="complete" heading="Job Success" content="This submission report has been generated successfully. [Submitted Attachments: 'CSC3002F Assignment 6']"></JobComponent>
 </div>
 <div className="key">
   <div>
