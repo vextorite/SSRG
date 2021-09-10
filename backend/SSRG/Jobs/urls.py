ff95e35fb45284ba9aae85e472df06fc03cc4d9b
vextorite <HLNSAN005@myuct.ac.za>
2021-09-10 18:16:46 +0200

    Revert back to Django FrontEnd

--- backend/SSRG/Jobs/urls.py
+++ backend/SSRG/Jobs/urls.py
@@ -5,7 +5,7 @@ from . import views
 appName = "Jobs"
 
 urlpatterns = [
-     path('', TemplateView.as_view(template_name='index.html'), name='homepage'),
+     path('', views.homepage, name='homepage'),
      path('register', views.registerRequest, name = 'register'),
      path('login', views.loginRequest, name='login'),
      path('logout', views.logoutRequest, name='logout'),
--- backend/SSRG/Jobs_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobsApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Jobs_api'
