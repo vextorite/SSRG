b5fc3e4be61b04c6cef7a3d3c7d6ed7493a0aca1
Suvanth Ramruthen <rmrsuv002@cs.uct.ac.za>
2021-09-16 16:19:02 +0200

    Demo ready, needs comments

---
+++ MossBackendJobs/heatV2.py
@@ -0,0 +1,81 @@
+from processor import scrapeUrl
+from typing_extensions import final
+import numpy
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from seaborn.distributions import histplot;
+from reportGenerator import generateReport
+
+
+def populateDataList(urlLink):
+    dataList=[]
+    histList=[]
+    finalList=[]
+    rawList = scrapeUrl(urlLink)
+    rawList.pop(0)
+    for i in rawList:
+        currentRecord=[]
+        student1 = i[0]
+        student2 = i[1]
+        student1Index = student1[student1.rfind('/')+1:student1.rfind('.')]
+        currentRecord.append(student1Index.strip())
+        student1Percent = student1[student1.find('(')+1:student1.find(')')-1]
+        currentRecord.append(student1Percent.strip())
+        student2Index = student2[student2.rfind('/')+1:student2.find('.')]
+        currentRecord.append(student2Index.strip())
+        student2Percent = student2[student2.find('(')+1:student2.find(')')-1]
+        currentRecord.append(student2Percent.strip())
+        currentRecord.append(i[2])
+        dataList.append(currentRecord)
+        histList.append(int(i[2]))
+    finalList.append(dataList)
+    finalList.append(histList)
+    return finalList
+
+def studentList(exportPathHeat, exportPathHist,url):
+    total=0
+    studentList ={}
+    studentList = set()
+    myFilteredList =[]
+    mylst = populateDataList(url)
+    for i in mylst[0]:
+        total=total+int(i[4])
+        if int(i[4])>=50:
+            myFilteredList.append(i)
+            studentList.add(i[0])
+            studentList.add(i[2])
+    
+    init_arr=numpy.zeros((len(studentList),len(studentList)))
+    df3 = pd.DataFrame(init_arr, index= studentList, columns=studentList)
+    for i in myFilteredList:
+        print(f'{i[0]}  and {i[1]} and {i[2]} and {i[3]}')
+        df3[i[0]][i[2]] = i[1]
+        df3[i[2]][i[0]] = i[3]
+    #print(df3)
+    sns.heatmap(df3, cmap='rocket_r', annot=True, fmt='.0f',  ).set_title("MOSS High Average Lines Matched Heatmap")
+    plt.title='Moss'
+    plt.tight_layout()
+    plt.savefig(exportPathHeat)
+    plt.close()
+    print('heat done')
+
+    histlist = mylst[1]
+    histlist.sort()
+    for i in histlist:
+        print(i)
+    sns.histplot(histlist, color= 'Blue', bins=[20,40,60,80,100,120,180,190] )
+    plt.title='Distribution of Average Matched Lines'
+    plt.xlabel("Average lines matched categories", fontsize =12)
+    plt.ylabel("Observations", fontsize =12)
+    plt.tight_layout()
+    plt.savefig(exportPathHist)
+    print('hist done')
+
+
+
+def completeReport(jobId,url):
+    studentList(f'/Users/suvanth/Desktop/test/Suvanth/{jobId}heatmap.png', f'/Users/suvanth/Desktop/test/Suvanth/{jobId}histogram.png', url )
+    print('check test for your pics')
+    generateReport(url, f'/Users/suvanth/Desktop/test/Suvanth/{jobId}report.pdf', f'/Users/suvanth/Desktop/test/Suvanth/{jobId}heatmap.png', f'/Users/suvanth/Desktop/test/Suvanth/{jobId}histogram.png')
+    print('check test for your pdfs')
---
+++ MossBackendJobs/reportGenerator.py
@@ -0,0 +1,69 @@
+from processor import scrapeUrl
+from fpdf import FPDF
+
+class PDF(FPDF):
+    def header(self):
+        #logo 
+        self.image("logo.png", 10, 8, 25)
+        #just setting width allows for auto height that wont distort
+        # path, x coord, y coord, width
+        self.set_font('helvetica', 'B', 20)
+        #title
+        self.cell(80)
+        self.cell(30, 10, 'MOSS Similarity Report', border=False, ln=True, align='C')
+        self.ln(20)
+
+        return super().header()
+
+    def footer(self):
+        self.set_y(-15) # 15 mm from the bottom
+        self.set_font('helvetica', 'I', 10)
+        #page number
+        self.cell(0,10, f'Page {self.page_no()}/{{nb}}', align='C')
+        return super().footer()
+    
+    def report_title(self, re_num, re_title):
+        # set font
+        self.set_font('helvetica', '', 12)
+        # background color
+        self.set_fill_color(200, 220, 255)
+        # Chapter title
+        report_title = f'Output {re_num} : {re_title}'
+        self.cell(0, 5, report_title, ln=1, fill=1)
+        # line break
+        self.ln()
+
+
+def generateReport(url, pathName, heatPngPath, histPngPath):
+    myList = scrapeUrl(url)
+    pdf = PDF()
+    # metadata
+    print('hello')
+    pdf.set_title("MOSS REPORT")
+    pdf.set_author('SSRG Team')
+    pdf.add_page()
+    pdf.report_title(1, "Job Summary")
+    pdf.ln(10)
+    pdf.alias_nb_pages()
+    # add a page
+    pdf.set_auto_page_break(auto=True, margin=15) # how far from the bottom of the page will the page break be added
+    #specify font
+    pdf.set_font('Times', '', 8)
+    #table code
+    line_height = pdf.font_size * 2.5
+    col_width = pdf.epw / 3  # distribute content evenly
+    for row in myList:
+        for x in row:
+            if '/' in x:
+                val = x[x.rfind('/')+1:]
+            else:
+                val = x
+            pdf.multi_cell(col_width, line_height, val, border=1, ln=3, max_line_height=pdf.font_size)
+        pdf.ln(line_height)
+    pdf.add_page()
+    pdf.report_title(2, "Heatmap of high average matches")
+    pdf.image(heatPngPath, 50, 50, 100)
+    pdf.add_page()
+    pdf.report_title(2, "Histogram of the distribution of average lines matched")
+    pdf.image(histPngPath, 50, 50, 100)
+    pdf.output(pathName)
