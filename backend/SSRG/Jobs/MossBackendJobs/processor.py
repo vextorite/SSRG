283cb68dc70b00063fc4db694786c9314836a109
Sashen Naidoo <ndxsas021@cs.uct.ac.za>
2021-10-06 17:50:55 +0200

    final try

--- backend/SSRG/Jobs/MossBackendJobs/processor.py
+++ backend/SSRG/Jobs/MossBackendJobs/processor.py
@@ -92,15 +92,14 @@ def saveLinks(url, fRoot, bounds):
     file.write(webContent)
     file.close
 
-    #file1 = codecs.open(exportPath0, "r", "utf-8")
-    #htmlString1 = file1.read()
-    #file1.close()
+    file1 = codecs.open(exportPath0, "r", "utf-8")
+    html1String = file1.read()
+    file1.close()
 
-    #file2 = codecs.open(exportPath1, "r", "utf-8")
-    #htmlString2 = file2.read()
-    #file2.close()
+    file2 = codecs.open(exportPath1, "r", "utf-8")
+    html2String = file2.read()
+    file2.close()
 
-    html1String="1"
-    html2String="2"
+  
 
     comparissonFrameConstructor(html1String, html2String, i, fRoot)
